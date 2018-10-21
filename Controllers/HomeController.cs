f1c91f6b0c0716b32b18f8fbfbf09b5b8cdb9c9e
Kim Sangyeon <dho113@naver.com>
2018-10-21 10:36:08 +0900

    uploadFile/ uploadFile httpPost 연결 - file upload 로직 추가

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Web;
+
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using asp_editor_server.Models;
 
 namespace asp_editor_server.Controllers
@@ -19,6 +23,24 @@ namespace asp_editor_server.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost("uploadFile")]
+        public async Task<IActionResult> Post(List<IFormFile> file)
+        {
+            long size = file.Sum(f => f.Length);
+            var uploadFile = file[0];
+            string fileName = Path.GetFileName(uploadFile.FileName);
+            string ext = Path.GetExtension(fileName);
+            var filePath = Path.Combine("uploads", fileName);
+            var uploadUrl = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await uploadFile.CopyToAsync(stream);
+            }
+
+            return Ok(new { count = file.Count, size, filePath });
         }
     }
 }
